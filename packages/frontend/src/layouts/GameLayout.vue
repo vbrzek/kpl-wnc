0dd318f91a1e279439837d6058c736ae11bf8584
vbrzek <165887224+vbrzek@users.noreply.github.com>
2026-02-26 19:23:38 +0100

    feat(frontend): replace LanguageSwitcher with PlayerAvatar in GameLayout

--- packages/frontend/src/layouts/GameLayout.vue
+++ packages/frontend/src/layouts/GameLayout.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
-import LanguageSwitcher from '../components/LanguageSwitcher.vue';
+import { ref } from 'vue';
+import PlayerAvatar from '../components/PlayerAvatar.vue';
+import PlayerProfileModal from '../components/PlayerProfileModal.vue';
+
+const showEditProfile = ref(false);
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-900 text-white">
     <div class="flex justify-end px-6 pt-4">
-      <LanguageSwitcher />
+      <PlayerAvatar :size="40" @click="showEditProfile = true" />
     </div>
     <div class="max-w-6xl mx-auto px-6 py-6">
       <slot></slot>
     </div>
+
+    <PlayerProfileModal
+      v-if="showEditProfile"
+      :is-edit="true"
+      @close="showEditProfile = false"
+    />
   </div>
 </template>
