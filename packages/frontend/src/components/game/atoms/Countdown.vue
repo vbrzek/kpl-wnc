2b43fcefa000938b43e47f5286fa352708d5fe1f
vbrzek <165887224+vbrzek@users.noreply.github.com>
2026-02-26 09:31:07 +0100

    elegant timer

--- packages/frontend/src/components/game/atoms/Countdown.vue
+++ packages/frontend/src/components/game/atoms/Countdown.vue
@@ -6,16 +6,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-center gap-2">
-    <div class="flex-1 bg-gray-700 rounded-full h-2">
-      <div
-        class="h-2 rounded-full transition-all"
-        :class="secondsLeft <= 10 ? 'bg-red-500' : 'bg-yellow-400'"
-        :style="{ width: `${(secondsLeft / totalSeconds) * 100}%` }"
-      />
-    </div>
-    <span class="text-sm font-mono" :class="secondsLeft <= 10 ? 'text-red-400' : 'text-gray-300'">
-      {{ secondsLeft }}s
-    </span>
+  <div class="fixed top-0 left-0 right-0 z-50 h-0.5 bg-transparent">
+    <div
+      class="h-full transition-[width] duration-1000 ease-linear origin-left"
+      :class="secondsLeft <= 10 ? 'bg-red-500' : 'bg-yellow-400'"
+      :style="{ width: `${(secondsLeft / totalSeconds) * 100}%` }"
+    />
   </div>
 </template>
