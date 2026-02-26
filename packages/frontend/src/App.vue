db3465d732666afb0e274db8d55a2e5de27e6c28
vbrzek <165887224+vbrzek@users.noreply.github.com>
2026-02-26 19:23:13 +0100

    feat(frontend): init profileStore in App.vue, block RouterView until profile is set

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import { socket } from './socket';
 import GameLayout from './layouts/GameLayout.vue';
+import { useProfileStore } from './stores/profileStore';
+import PlayerProfileModal from './components/PlayerProfileModal.vue';
 
-onMounted(() => socket.connect());
+const profileStore = useProfileStore();
+const showProfileModal = ref(false);
+
+onMounted(() => {
+  socket.connect();
+  profileStore.init();
+  if (!profileStore.hasProfile) showProfileModal.value = true;
+});
 onUnmounted(() => socket.disconnect());
 </script>
 
 <template>
   <GameLayout>
-      <RouterView />
+    <!-- RouterView se renderuje jen pokud profil existuje -->
+    <RouterView v-if="profileStore.hasProfile" />
   </GameLayout>
+
+  <PlayerProfileModal
+    v-if="showProfileModal"
+    @close="showProfileModal = false"
+  />
 </template>
